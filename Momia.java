e5c92ea41c6b57014657501b6a0dbf2f8fb1f6c9
laurent010217-cell <laurent010217@gmail.com>
2026-05-03 19:01:04 -0600

    feat: implementado sistema de ataque y velocidad de la momia

--- Momia.java
+++ Momia.java
@@ -1,17 +1,29 @@
 public class Momia extends Entidad {
+    private int velocidad;
+
     public Momia(String nombre, int x, int y) {
         super(nombre, x, y);
+        this.velocidad = 1; // puedes ajustar la velocidad
+    }
+
+    public void atacar(Entidad objetivo) {
+        System.out.println("🧟 " + nombre + " ataca a " + objetivo.getNombre());
+    }
+
+    public void perseguir() {
+        y += velocidad;
+        System.out.println("🧟 " + nombre + " se mueve a: [" + x + "," + y + "]");
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                // La momia se mueve cada 1.5 segundos (hilo rápido)
-                y++; 
-                System.out.println("🧟 " + nombre + " acecha en: [" + x + "," + y + "]");
+                perseguir(); // usa tu nueva lógica
                 Thread.sleep(1500); 
-            } catch (InterruptedException e) { break; }
+            } catch (InterruptedException e) { 
+                break; 
+            }
         }
     }
 }
