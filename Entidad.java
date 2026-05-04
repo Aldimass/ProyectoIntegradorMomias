b376819aa00483e581477e6354d28d332b4b76ed
iAxelS <a340491240@my.uvm.edu.mx>
2026-05-03 21:37:54 -0600

    Update Entidad.java
    docs: definida estructura abstracta de Entidad y atributos base

--- Entidad.java
+++ Entidad.java
@@ -1,10 +1,31 @@
-public abstract class Entidad extends Thread {
+public abstract class Entidad {
+    protected int x, y;
     protected String nombre;
-    protected int x, y; // Coordenadas de posición
+    protected int salud;
 
-    public Entidad(String nombre, int x, int y) {
+    public Entidad(String nombre, int x, int y, int salud) {
         this.nombre = nombre;
         this.x = x;
         this.y = y;
+        this.salud = salud;
     }
+
+    // Métodos comunes
+    public void moverse(int nuevaX, int nuevaY) {
+        this.x = nuevaX;
+        this.y = nuevaY;
+    }
+
+    // Getters y Setters
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
+    public String getNombre() { return nombre; }
+    public int getSalud() { return salud; }
+    public void setSalud(int salud) { this.salud = salud; }
+
+    // Método que obliga a los demás a implementar su lógica
+    public abstract void actuar();
 }
+
