c227db1fbb1744955dbd49aaa6f7433aea092761
iAxelS <a340491240@my.uvm.edu.mx>
2026-05-03 19:49:32 -0600

    Update Arqueologo.java
    feat: implementada logica de supervivencia y consumo de aceite

--- Arqueologo.java
+++ Arqueologo.java
@@ -1,28 +1,29 @@
 public class Arqueologo extends Entidad {
-    private Aceite lampara; // Relación: el arqueólogo USA el aceite
+    private boolean estaVivo;
+    private int nivelAceiteLampara;
+    private RecursoCompartido recurso; // Asociación
 
-    public Arqueologo(String nombre, int x, int y, Aceite lampara) {
-        super(nombre, x, y);
-        this.lampara = lampara;
+    public Arqueologo(String nombre, RecursoCompartido recurso) {
+        super(nombre, 0, 0, 100);
+        this.recurso = recurso;
+        this.estaVivo = true;
+        this.nivelAceiteLampara = 20; // Inicia con algo de aceite
     }
 
-    // El método "run" es el cuerpo del hilo
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                // 1. Gasta aceite antes de moverse
-                if (lampara.consumir(10)) {
-                    x++; // 2. Avanza un paso
-                    System.out.println("🚶 " + nombre + " en: [" + x + "," + y + "]");
-                    // 3. Espera 2 segundos (hilo lento)
-                    Thread.sleep(2000); 
-                } else {
-                    // 4. Si no hay aceite, el hilo termina (muere)
-                    System.out.println("💀 " + nombre + " se quedó sin luz... GAME OVER.");
-                    break; 
-                }
-            } catch (InterruptedException e) { break; }
+    public void huir() {
+        if (recurso.consumirAceite()) {
+            this.x += 5;
+            System.out.println(nombre + " huyó. Aceite restante en depósito.");
+        } else {
+            System.out.println("¡Sin aceite! " + nombre + " no puede correr.");
         }
     }
+
+    @Override
+    public void actuar() {
+        System.out.println(nombre + " está explorando la pirámide...");
+    }
+
+    public int getNivelAceite() { return nivelAceiteLampara; }
 }
+
