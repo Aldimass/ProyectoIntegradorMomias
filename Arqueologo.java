5fc81f7a8d8c516a4ad03d946842ec732ca7fc94
iAxelS <a340491240@my.uvm.edu.mx>
2026-06-21 19:36:03 -0600

    Update Arqueologo.java
    Actualizado

--- Arqueologo.java
+++ Arqueologo.java
@@ -1,29 +1,39 @@
-public class Arqueologo extends Entidad {
-    private boolean estaVivo;
-    private int nivelAceiteLampara;
-    private RecursoCompartido recurso; // Asociación
+package uvm;
+import java.awt.Color;
 
-    public Arqueologo(String nombre, RecursoCompartido recurso) {
-        super(nombre, 0, 0, 100);
-        this.recurso = recurso;
-        this.estaVivo = true;
-        this.nivelAceiteLampara = 20; // Inicia con algo de aceite
+public class Arqueologo {
+    private int arqueologoX;
+    private int arqueologoY;
+    private int tamano;
+    private Color cuerpo;
+    private float aceite; 
+
+    public Arqueologo(int xInicio, int yInicio) {
+        this.arqueologoX = xInicio;
+        this.arqueologoY = yInicio;
+        this.tamano = 12; // Personaje pequeño para caber en el laberinto
+        this.cuerpo = new Color(139, 69, 19); 
+        this.aceite = 100.0f; 
     }
 
-    public void huir() {
-        if (recurso.consumirAceite()) {
-            this.x += 5;
-            System.out.println(nombre + " huyó. Aceite restante en depósito.");
-        } else {
-            System.out.println("¡Sin aceite! " + nombre + " no puede correr.");
-        }
+    public int getJugadorX() { return arqueologoX; }
+    public void setJugadorX(int x) { this.arqueologoX = x; }
+    
+    public int getJugadorY() { return arqueologoY; }
+    public void setJugadorY(int y) { this.arqueologoY = y; }
+    
+    public int getTamano() { return tamano; }
+    public Color getCuerpo() { return cuerpo; }
+    public float getAceite() { return aceite; }
+    public void setAceite(float aceite) { this.aceite = aceite; }
+
+    public void consumirAceite(float cantidad) {
+        this.aceite -= cantidad;
+        if (this.aceite < 0) this.aceite = 0;
     }
 
-    @Override
-    public void actuar() {
-        System.out.println(nombre + " está explorando la pirámide...");
+    public void recargarAceite() {
+        this.aceite += 40.0f; 
+        if (this.aceite > 100.0f) this.aceite = 100.0f;
     }
-
-    public int getNivelAceite() { return nivelAceiteLampara; }
 }
-
